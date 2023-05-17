4e7671aa1b099cef4e3cd6be00f353080b421c6e
Alex Khomenok <exleonardo@mail.ru>
2023-05-17 21:44:45 +0300

    structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
 
 const App = () => {
-	return <div></div>;
+	return (
+		<div className="app-wrapper">
+			<header>
+				<img src="https://andpro.ru/upload/iblock/984/Apple_logo_black.svg" width={400} height={400} />
+			</header>
+			<nav>
+				<div>Profile</div>
+				<div>Messages</div>
+				<div>News</div>
+				<div>Music</div>
+			</nav>
+			<div>main content</div>
+		</div>
+	);
 };
 
 export default App;
