e2f8f5e95a383ea28686415fad90199d1267b49e
Alex Khomenok <exleonardo@mail.ru>
2023-05-17 22:09:28 +0300

    add css

--- src/App.js
+++ src/App.js
@@ -3,16 +3,16 @@ import "./App.css";
 const App = () => {
 	return (
 		<div className="app-wrapper">
-			<header>
-				<img src="https://andpro.ru/upload/iblock/984/Apple_logo_black.svg" width={400} height={400} />
+			<header className="header">
+				<img src="https://andpro.ru/upload/iblock/984/Apple_logo_black.svg" />
 			</header>
-			<nav>
+			<nav className="nav">
 				<div>Profile</div>
 				<div>Messages</div>
 				<div>News</div>
 				<div>Music</div>
 			</nav>
-			<div>main content</div>
+			<div className="content">main content</div>
 		</div>
 	);
 };
