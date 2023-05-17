3340576340c4488206f7a6f1740d11985c3a2473
Alex Khomenok <exleonardo@mail.ru>
2023-05-17 20:45:53 +0300

    del components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
 import "./App.css";
-
+import Technologies from "./Technologies";
 const App = () => {
 	return (
 		<div>
 			<Header />
 			<Technologies />
-		</div>
-	);
-};
-const Technologies = () => {
-	return (
-		<div>
-			<ul>
-				<li>Name</li>
-				<li>html</li>
-				<li>js</li>
-				<li>React</li>
-			</ul>
 		</div>
 	);
 };
---
+++ src/Technologies.js
@@ -0,0 +1,13 @@
+const Technologies = () => {
+	return (
+		<div>
+			<ul>
+				<li>Name</li>
+				<li>html</li>
+				<li>js</li>
+				<li>React</li>
+			</ul>
+		</div>
+	);
+};
+export default Technologies;
