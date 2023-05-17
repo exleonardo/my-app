a332b77e175112a2eb35ff94782922e8c88edb95
Alex Khomenok <exleonardo@mail.ru>
2023-05-17 20:51:47 +0300

    разбивка

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
 import "./App.css";
 import Technologies from "./Technologies";
+import Header from "./Header";
 const App = () => {
 	return (
 		<div>
 			<Header />
 			<Technologies />
-		</div>
-	);
-};
-
-export const Header = () => {
-	return (
-		<div>
-			<a href="#S"> LOL</a>
-			<a href="#S"> html</a>
-			<a href="#S"> js</a>
-			<a href="#S"> React</a>
 		</div>
 	);
 };
---
+++ src/Header.js
@@ -0,0 +1,12 @@
+const Header = () => {
+	return (
+		<div>
+			<a href="#S"> LOL</a>
+			<a href="#S"> html</a>
+			<a href="#S"> js</a>
+			<a href="#S"> React</a>
+		</div>
+	);
+};
+
+export default Header;
