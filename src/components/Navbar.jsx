b4fc1c0a73cfb6ec3c07a7f6f2d8f8bd35703830
Alex Khomenok <exleonardo@mail.ru>
2023-05-18 14:31:43 +0300

    add styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,19 @@
 const Navbar = () => {
 	return (
 		<nav className="nav">
-			<div>
+			<div className="item">
 				<a href="#s">Profile</a>
 			</div>
-			<div>
+			<div className="item">
 				<a href="#s">Messages</a>
 			</div>
-			<div>
+			<div className="item">
 				<a href="#s">News</a>
 			</div>
-			<div>
+			<div className="item">
 				<a href="#s">Music</a>
 			</div>
-			<div>
+			<div className="item">
 				<a href="#s">Settings</a>
 			</div>
 		</nav>
