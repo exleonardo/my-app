4f462b10afed39676af7c83156fdf5c496903f89
Alex Khomenok <exleonardo@mail.ru>
2023-05-18 19:18:38 +0300

    add variable

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -9,8 +9,8 @@ const MyPosts = () => {
 				<button>Add Post</button>
 			</div>
 			<div className={s.posts}>
-				<Post />
-				<Post />
+				<Post message="Hi how are you " />
+				<Post message="It's my first post" />
 			</div>
 		</div>
 	);
--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,9 +1,10 @@
 import s from "./Post.module.css";
-const Post = () => {
+const Post = (props) => {
+	const message = props.message;
 	return (
 		<div className={s.item}>
 			<img src="https://www.meme-arsenal.com/memes/df2292c6c154dbedf5b22ab0fc54c755.jpg" alt="logo" />
-			post 1
+			{message}
 			<div>
 				<span>Like</span>
 			</div>
