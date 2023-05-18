bf60463548998d24e64f96e5c93210a119517fb7
Alex Khomenok <exleonardo@mail.ru>
2023-05-18 13:57:33 +0300

    add component

--- src/App.js
+++ src/App.js
@@ -1,42 +1,14 @@
 import "./App.css";
 import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Profile from "./components/Profile";
 
 const App = () => {
 	return (
 		<div className="app-wrapper">
 			<Header />
-			<nav className="nav">
-				<div>
-					<a href="#s">Profile</a>
-				</div>
-				<div>
-					<a href="#s">Messages</a>
-				</div>
-				<div>
-					<a href="#s">News</a>
-				</div>
-				<div>
-					<a href="#s">Music</a>
-				</div>
-				<div>
-					<a href="#s">Settings</a>
-				</div>
-			</nav>
-			<div className="content">
-				<div>
-					<img src="https://images.theconversation.com/files/71098/original/image-20150204-28578-7qf35.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=1200.0&fit=crop"></img>
-				</div>
-				<div>ava + desc</div>
-				<div>
-					my posts
-					<div>new Post</div>
-					<div>
-						posts
-						<div>post 1</div>
-						<div>post 2</div>
-					</div>
-				</div>
-			</div>
+			<Navbar />
+			<Profile />
 		</div>
 	);
 };
---
+++ src/components/Profile.jsx
@@ -0,0 +1,23 @@
+const Profile = () => {
+	return (
+		<div className="content">
+			<div>
+				<img
+					src="https://images.theconversation.com/files/71098/original/image-20150204-28578-7qf35.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=1200.0&fit=crop"
+					alt="logo"
+				></img>
+			</div>
+			<div>ava + desc</div>
+			<div>
+				my posts
+				<div>new Post</div>
+				<div>
+					posts
+					<div>post 1</div>
+					<div>post 2</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+export default Profile;
