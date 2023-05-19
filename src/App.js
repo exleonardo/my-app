1c5323ac05d41b2207e35c2d777d65c4cee2d93d
Alex Khomenok <exleonardo@mail.ru>
2023-05-19 12:11:31 +0300

    add route

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Dialogs from "./components/Dialogs/Dialogs";
 import Header from "./components/Header/Header";
@@ -6,14 +7,19 @@ import Profile from "./components/Profile/Profile";
 
 const App = () => {
 	return (
-		<div className="app-wrapper">
-			<Header />
-			<Navbar />
-			<div className="app-wrapper-content">
-				{/* <Profile /> */}
-				<Dialogs />
+		<BrowserRouter>
+			<div className="app-wrapper">
+				<Header />
+				<Navbar />
+
+				<div className="app-wrapper-content">
+					<Routes>
+						<Route path="/profile/*" element={<Profile />} />
+						<Route path="/dialogs/*" element={<Dialogs />} />
+					</Routes>
+				</div>
 			</div>
-		</div>
+		</BrowserRouter>
 	);
 };
 
