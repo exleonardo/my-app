3da5910cf4c480e679c481c280aa07ae4fbfa0f7
Alex Khomenok <exleonardo@mail.ru>
2023-05-20 10:10:03 +0300

    add profileInfo

---
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -0,0 +1,16 @@
+import s from "./ProfileInfo.module.css";
+const ProfileInfo = (props) => {
+	return (
+		<div>
+			<div>
+				<img
+					src="https://images.theconversation.com/files/71098/original/image-20150204-28578-7qf35.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=1200.0&fit=crop"
+					alt="logo"
+				></img>
+			</div>
+			<div>ava + desc</div>
+		</div>
+	);
+};
+
+export default ProfileInfo;
