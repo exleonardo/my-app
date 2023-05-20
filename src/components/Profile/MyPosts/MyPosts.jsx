358e8fa9ee9947cdc0247b7b47383a37c4a3e7c2
Alex Khomenok <exleonardo@mail.ru>
2023-05-20 11:21:05 +0300

    add postDate

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,6 +1,10 @@
 import s from "./MyPosts.module.css";
 import Post from "./Post/Post";
 const MyPosts = () => {
+	let postDate = [
+		{ id: 1, message: "Hi how are you ", likesCount: "1" },
+		{ id: 2, message: "It's my first post", likesCount: "2" },
+	];
 	return (
 		<div className={s.postsBlock}>
 			<h3>my posts</h3>
@@ -13,8 +17,8 @@ const MyPosts = () => {
 				</div>
 			</div>
 			<div className={s.posts}>
-				<Post message="Hi how are you " likesCount="1" />
-				<Post message="It's my first post" likesCount="2" />
+				<Post message={postDate[0].message} id={postDate[0].id} likesCount={postDate[0].likesCount} />
+				<Post message={postDate[1].message} id={postDate[1].id} likesCount={postDate[1].likesCount} />
 			</div>
 		</div>
 	);
