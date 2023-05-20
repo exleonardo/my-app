6ded069dbd0af1889b06ae98e5f4a6fc083e1e7a
Alex Khomenok <exleonardo@mail.ru>
2023-05-20 11:03:38 +0300

    add dialogsData

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -13,14 +13,21 @@ const Dialogs = (props) => {
 	const Message = (props) => {
 		return <div className={s.message}>{props.message}</div>;
 	};
+	const dialogsData = [
+		{ id: 1, name: "Dima" },
+		{ id: 2, name: "Masha" },
+		{ id: 3, name: "Rita" },
+		{ id: 4, name: "Lera" },
+		{ id: 5, name: "Artem" },
+	];
 	return (
 		<div className={s.dialogs}>
 			<div className={s.dialogsItems}>
-				<DialigItem name="Dima" id="1" />
-				<DialigItem name="Masha" id="2" />
-				<DialigItem name="Rita" id="3" />
-				<DialigItem name="Lera" id="4" />
-				<DialigItem name="Artem" id="5" />
+				<DialigItem name={dialogsData[0].name} id={dialogsData[0].id} />
+				<DialigItem name={dialogsData[1].name} id={dialogsData[1].id} />
+				<DialigItem name={dialogsData[2].name} id={dialogsData[2].id} />
+				<DialigItem name={dialogsData[3].name} id={dialogsData[3].id} />
+				<DialigItem name={dialogsData[4].name} id={dialogsData[4].id} />
 			</div>
 			<div className={s.messages}>
 				<Message message="Hi" />
