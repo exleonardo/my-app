0c3e056178c00a5d8fe25a211d3da6d4e26d7171
Alex Khomenok <exleonardo@mail.ru>
2023-05-20 10:11:54 +0300

    add component

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,15 +1,11 @@
 import MyPosts from "./MyPosts/MyPosts";
 import s from "./Profile.module.css";
+import ProfileInfo from "./ProfileInfo/ProfileInfo";
+
 const Profile = () => {
 	return (
 		<div>
-			<div>
-				<img
-					src="https://images.theconversation.com/files/71098/original/image-20150204-28578-7qf35.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=1200.0&fit=crop"
-					alt="logo"
-				></img>
-			</div>
-			<div>ava + desc</div>
+			<ProfileInfo />
 			<MyPosts />
 		</div>
 	);
