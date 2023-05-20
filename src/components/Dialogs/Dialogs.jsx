ef7cb63be952c161755e27a0980778ed8eb6585f
Alex Khomenok <exleonardo@mail.ru>
2023-05-20 09:19:16 +0300

    add props name

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,6 +1,29 @@
 import s from "./Dialogs.module.css";
+import { Link } from "react-router-dom";
 
 const Dialogs = (props) => {
-	return <div>dialogs</div>;
+	const DialigItem = (props) => {
+		return (
+			<div className={s.dialog + ` ` + s.active}>
+				<Link to="/dialogs/1">{props.name}</Link>
+			</div>
+		);
+	};
+	return (
+		<div className={s.dialogs}>
+			<div className={s.dialogsItems}>
+				<DialigItem name="Dima" />
+				<DialigItem name="Masha" />
+				<DialigItem name="Rita" />
+				<DialigItem name="Lera" />
+				<DialigItem name="Artem" />
+			</div>
+			<div className={s.messages}>
+				<div className={s.message}>Hi</div>
+				<div className={s.message}>How is your it-camas</div>
+				<div className={s.message}>Yo!</div>
+			</div>
+		</div>
+	);
 };
 export default Dialogs;
