b059e1fcc5d3731a52e7070685e49c3daa52674e
Alex Khomenok <exleonardo@mail.ru>
2023-05-21 11:55:41 +0300

    change props

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ const App = (props) => {
 					<Routes>
 						<Route
 							path="/profile/*"
-							element={<Profile state={props.state.profilePage} addPost={props.addPost} />}
+							element={<Profile ProfilePage={props.state.profilePage} addPost={props.addPost} />}
 						/>
 						<Route path="/dialogs/*" element={<Dialogs state={props.state.dialogsPage} />} />
 						{/* <Route path="/news/*" element={<Dialogs />} />
