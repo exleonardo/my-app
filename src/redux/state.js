9641f588db37b2a2458135181e90017e647760ef
Alex Khomenok <exleonardo@mail.ru>
2023-05-22 11:58:29 +0300

    add state to store

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,4 +1,30 @@
-let store = {};
+let store = {
+	state: {
+		profilePage: {
+			posts: [
+				{ id: 1, message: "Hi how are you ", likesCount: 1 },
+				{ id: 2, message: "It's my first post", likesCount: 2 },
+			],
+			newPostText: "it-camasutra.com",
+		},
+		dialogsPage: {
+			messages: [
+				{ id: 1, message: "Hi" },
+				{ id: 2, message: "How is your it-camasutra" },
+				{ id: 3, message: "Yo!" },
+				{ id: 4, message: "Yo!" },
+				{ id: 5, message: "Yo!" },
+			],
+			dialogs: [
+				{ id: 1, name: "Dima" },
+				{ id: 2, name: "Masha" },
+				{ id: 3, name: "Rita" },
+				{ id: 4, name: "Lera" },
+				{ id: 5, name: "Artem" },
+			],
+		},
+	},
+};
 
 let rerenderEntireThree = () => {};
 let state = {
