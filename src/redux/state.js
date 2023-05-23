dd315391932d9896879649ffed028323baf4a6f1
Exleonardo <exleonardo@mail.ru>
2023-05-23 19:44:06 +0300

    add private

--- src/redux/state.js
+++ src/redux/state.js
@@ -28,13 +28,13 @@ let store = {
 		return this._state;
 	},
 	_callSubscriber() {},
-	addPost() {
+	_addPost() {
 		let newPost = { id: 5, message: this._state.profilePage.newPostText, likesCount: 0 };
 		this._state.profilePage.posts.push(newPost);
 		this._state.profilePage.newPostText = ``;
 		this._callSubscriber(this._state);
 	},
-	updateNewPostText(newText) {
+	_updateNewPostText(newText) {
 		this._state.profilePage.newPostText = newText;
 		this._callSubscriber(this._state);
 	},
