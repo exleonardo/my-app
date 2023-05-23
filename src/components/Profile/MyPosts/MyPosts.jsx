a524f8bcd63d6bec5e6ab979061304dd1c5af686
Exleonardo <exleonardo@mail.ru>
2023-05-23 12:56:17 +0300

    add type : ADD_POST

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -7,11 +7,11 @@ const MyPosts = (props) => {
 	));
 	const newPostElement = React.createRef();
 	const addPost = () => {
-		props.addPost();
+		props.dispatch({ type: "ADD-POST" });
 	};
 	let onPostChange = () => {
 		let text = newPostElement.current.value;
-		props.updateNewPostText(text);
+		props.dispatch(text);
 	};
 	return (
 		<div className={s.postsBlock}>
