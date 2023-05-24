364c60384556c2646663e56697bbbcd07c8cdc25
Exleonardo <exleonardo@mail.ru>
2023-05-24 14:53:59 +0300

    add export

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import s from "./MyPosts.module.css";
 import Post from "./Post/Post";
 
-let addPostActionCreatter = () => {
+export let addPostActionCreatter = () => {
 	return { type: "ADD-POST" };
 };
 let updateNewPostTextActionCreator = (text) => {
