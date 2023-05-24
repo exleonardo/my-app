894f9f52ae5db991895a9f347c77c60044a782dd
Exleonardo <exleonardo@mail.ru>
2023-05-24 22:20:59 +0300

    add Message comp

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -7,18 +7,20 @@ const Dialogs = (props) => {
 	const dialogsElements = props.state.dialogs.map((d) => <DialogItem name={d.name} id={d.id} />);
 	const messagesElements = props.state.messages.map((m) => <Message message={m.message} id={m.id} />);
 	const newPostElement = React.createRef();
-	const addPost = () => {
-		let text = newPostElement.current.value;
-		alert(text);
-	};
+	const newMessageBody = props.state.newMessageBody.map((m) => <Message message={m.message} />);
+	const onSendMessageClick = () => {};
 	return (
 		<div className={s.dialogs}>
 			<div className={s.dialogsItems}>{dialogsElements}</div>
 			<div className={s.messages}>
-				{messagesElements}
-				<textarea ref={newPostElement}></textarea>
+				<div>{messagesElements}</div>
 				<div>
-					<button onClick={addPost}>add Post</button>
+					<div>
+						<textarea ref={newPostElement} placeholder="Enter your message"></textarea>
+					</div>
+					<div>
+						<button onClick={onSendMessageClick}>Send</button>
+					</div>
 				</div>
 			</div>
 		</div>
