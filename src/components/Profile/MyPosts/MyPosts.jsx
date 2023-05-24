5db1bb99c5d407f29a83930c6d942a886c4d7fb1
Exleonardo <exleonardo@mail.ru>
2023-05-24 14:57:34 +0300

    add import

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,13 +1,8 @@
 import React from "react";
 import s from "./MyPosts.module.css";
 import Post from "./Post/Post";
+import { addPostActionCreatter, updateNewPostTextActionCreator } from "../../../redux/state";
 
-export let addPostActionCreatter = () => {
-	return { type: "ADD-POST" };
-};
-export let updateNewPostTextActionCreator = (text) => {
-	return { type: "UPDATE-NEW-POST-TEXT", newText: text };
-};
 const MyPosts = (props) => {
 	const postsElements = props.posts.map((p) => (
 		<Post message={p.message} id={p.id} likesCount={p.likesCount} />
