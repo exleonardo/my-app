e307f50a792a875ee96dbbf3c11cf54fb742f112
Exleonardo <exleonardo@mail.ru>
2023-05-25 15:08:40 +0300

    add export

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -1,7 +1,7 @@
 const ADD_POST = "ADD-POST";
 const UPDATE_NEW_POST_TEXT = "UPDATE-NEW-POST-TEXT";
 
-const profileReducer = (state, action) => {
+export const profileReducer = (state, action) => {
 	if (action.type === ADD_POST) {
 		let newPost = { id: 5, message: this._state.profilePage.newPostText, likesCount: 0 };
 		state.posts.push(newPost);
