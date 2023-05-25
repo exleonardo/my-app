2728450fe0bd13133bdaf3c8ec8616e9b48f65a4
Exleonardo <exleonardo@mail.ru>
2023-05-25 15:10:44 +0300

    add export

--- src/redux/dialogs-reducer.js
+++ src/redux/dialogs-reducer.js
@@ -1,7 +1,7 @@
 const UPDATE_NEW_MESSAGE_BODY = "UPDATE-NEW-MESSAGE-BODY";
 const SEND_MESSAGE = "SEND-MESSAGE";
 
-const dialogsReducer = (state, action) => {
+export const dialogsReducer = (state, action) => {
 	if (action.type === UPDATE_NEW_MESSAGE_BODY) {
 		this._state.dialogsPage.newMessageBody = action.body;
 	} else if (action.type === SEND_MESSAGE) {
