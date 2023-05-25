efdb2683da2e3b201d1ebe58d10da9310cb265bd
Exleonardo <exleonardo@mail.ru>
2023-05-25 15:05:27 +0300

    add dialogsReducer

--- src/redux/dialogs-reducer.js
+++ src/redux/dialogs-reducer.js
@@ -1,3 +1,13 @@
+const UPDATE_NEW_MESSAGE_BODY = "UPDATE-NEW-MESSAGE-BODY";
+const SEND_MESSAGE = "SEND-MESSAGE";
+
 const dialogsReducer = (state, action) => {
+	if (action.type === UPDATE_NEW_MESSAGE_BODY) {
+		this._state.dialogsPage.newMessageBody = action.body;
+	} else if (action.type === SEND_MESSAGE) {
+		let body = this._state.dialogsPage.newMessageBody;
+		this._state.dialogsPage.newMessageBody = ``;
+		this._state.dialogsPage.messages.push({ id: 6, message: body });
+	}
 	return state;
 };
