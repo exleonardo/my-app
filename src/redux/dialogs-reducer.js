e3c6b940da75ab3453ff603057f256e91bcfdea1
Exleonardo <exleonardo@mail.ru>
2023-05-25 15:07:13 +0300

    add rev

--- src/redux/dialogs-reducer.js
+++ src/redux/dialogs-reducer.js
@@ -5,7 +5,7 @@ const dialogsReducer = (state, action) => {
 	if (action.type === UPDATE_NEW_MESSAGE_BODY) {
 		this._state.dialogsPage.newMessageBody = action.body;
 	} else if (action.type === SEND_MESSAGE) {
-		let body = this._state.dialogsPage.newMessageBody;
+		let body = state.newMessageBody;
 		state.newMessageBody = ``;
 		state.messages.push({ id: 6, message: body });
 	}
