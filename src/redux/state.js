e2fb228a3bb3dbc859937917938d646fd841a546
Exleonardo <exleonardo@mail.ru>
2023-05-25 15:21:34 +0300

    reducer

--- src/redux/state.js
+++ src/redux/state.js
@@ -48,23 +48,7 @@ let store = {
 		this._state.dialogsPage = dialogsReducer(this._state.dialogsPage, action);
 		this._state.sidebar = sidebarReducer(this._state.sidebar, action);
 
-		if (action.type === ADD_POST) {
-			let newPost = { id: 5, message: this._state.profilePage.newPostText, likesCount: 0 };
-			this._state.profilePage.posts.push(newPost);
-			this._state.profilePage.newPostText = ``;
-			this._callSubscriber(this._state);
-		} else if (action.type === UPDATE_NEW_POST_TEXT) {
-			this._state.profilePage.newPostText = action.newText;
-			this._callSubscriber(this._state);
-		} else if (action.type === UPDATE_NEW_MESSAGE_BODY) {
-			this._state.dialogsPage.newMessageBody = action.body;
-			this._callSubscriber(this._state);
-		} else if (action.type === SEND_MESSAGE) {
-			let body = this._state.dialogsPage.newMessageBody;
-			this._state.dialogsPage.newMessageBody = ``;
-			this._state.dialogsPage.messages.push({ id: 6, message: body });
-			this._callSubscriber(this._state);
-		}
+		this._callSubscriber(this._state);
 	},
 };
 
