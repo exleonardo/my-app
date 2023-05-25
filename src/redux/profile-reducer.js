b5903f17cfacf2c09013b6984df1cba37817ba77
Exleonardo <exleonardo@mail.ru>
2023-05-25 14:57:40 +0300

    add new func

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -1,3 +1,16 @@
+const ADD_POST = "ADD-POST";
+const UPDATE_NEW_POST_TEXT = "UPDATE-NEW-POST-TEXT";
+
 const profileReducer = (state, action) => {
+	if (action.type === ADD_POST) {
+		let newPost = { id: 5, message: this._state.profilePage.newPostText, likesCount: 0 };
+		this._state.profilePage.posts.push(newPost);
+		this._state.profilePage.newPostText = ``;
+		this._callSubscriber(this._state);
+	} else if (action.type === UPDATE_NEW_POST_TEXT) {
+		this._state.profilePage.newPostText = action.newText;
+		this._callSubscriber(this._state);
+	}
+
 	return state;
 };
