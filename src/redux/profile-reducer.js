5660aba12e3fca760555509666636ea1dfb051e7
Exleonardo <exleonardo@mail.ru>
2023-05-25 15:22:55 +0300

    review

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -3,7 +3,7 @@ const UPDATE_NEW_POST_TEXT = "UPDATE-NEW-POST-TEXT";
 
 export const profileReducer = (state, action) => {
 	if (action.type === ADD_POST) {
-		let newPost = { id: 5, message: this._state.profilePage.newPostText, likesCount: 0 };
+		let newPost = { id: 5, message: state.newPostText, likesCount: 0 };
 		state.posts.push(newPost);
 		state.newPostText = ``;
 	} else if (action.type === UPDATE_NEW_POST_TEXT) {
