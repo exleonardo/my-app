405ed62f56df4d2dbe6ad3afc922b58807013dd0
Exleonardo <exleonardo@mail.ru>
2023-05-27 11:46:26 +0300

    addd container

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,4 +1,5 @@
 import MyPosts from "./MyPosts/MyPosts";
+import MyPostsContainer from "./MyPosts/MyPostsContainer";
 import s from "./Profile.module.css";
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
 
@@ -6,7 +7,8 @@ const Profile = (props) => {
 	return (
 		<div>
 			<ProfileInfo />
-			<MyPosts
+			<MyPostsContainer
+				store={props.store}
 				profilePage={props.profilePage}
 				posts={props.profilePage.posts}
 				newPostText={props.profilePage.newPostText}
