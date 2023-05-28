c2bfdb8b25415a621d184b9bcb62fca3a4a6aafd
Exleonardo <exleonardo@mail.ru>
2023-05-28 13:29:45 +0300

    fix bug profileCont

--- src/components/Profile/MyPosts/MyPostsContainer.jsx
+++ src/components/Profile/MyPosts/MyPostsContainer.jsx
@@ -4,23 +4,24 @@ import MyPosts from "./MyPosts";
 import StoreContext from "../../../storeContext";
 
 const MyPostsContainer = (props) => {
-	let onPostChange = (text) => {
-		let action = updateNewPostTextActionCreator(text);
-		props.store.dispatch(action);
-	};
 	return (
 		<StoreContext.Consumer>
 			{(store) => {
-				let state = props.store.getState();
+				debugger;
+				let state = store.getState();
 				const addPost = () => {
-					props.store.dispatch(addPostActionCreatter());
+					store.dispatch(addPostActionCreatter());
+				};
+				let onPostChange = (text) => {
+					let action = updateNewPostTextActionCreator(text);
+					store.dispatch(action);
 				};
 				return (
 					<MyPosts
 						updateNewPostText={onPostChange}
 						addPost={addPost}
-						posts={state.getState().profilePage.posts}
-						newPostText={state.getState().profilePage.newPostText}
+						posts={state.profilePage.posts}
+						newPostText={state.profilePage.newPostText}
 					/>
 				);
 			}}
