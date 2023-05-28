7d3bd276c9e12310aa6460fc451c09a0749ad87a
Exleonardo <exleonardo@mail.ru>
2023-05-28 21:58:26 +0300

    fix cod

--- src/components/Profile/MyPosts/MyPostsContainer.jsx
+++ src/components/Profile/MyPosts/MyPostsContainer.jsx
@@ -1,34 +1,8 @@
 import React from "react";
 import { addPostActionCreatter, updateNewPostTextActionCreator } from "../../../redux/profile-reducer";
 import MyPosts from "./MyPosts";
-import StoreContext from "../../../storeContext";
 import { connect } from "react-redux";
 
-const MyPostsContainer = (props) => {
-	return (
-		<StoreContext.Consumer>
-			{(store) => {
-				debugger;
-				let state = store.getState();
-				const addPost = () => {
-					store.dispatch(addPostActionCreatter());
-				};
-				let onPostChange = (text) => {
-					let action = updateNewPostTextActionCreator(text);
-					store.dispatch(action);
-				};
-				return (
-					<MyPosts
-						updateNewPostText={onPostChange}
-						addPost={addPost}
-						posts={state.profilePage.posts}
-						newPostText={state.profilePage.newPostText}
-					/>
-				);
-			}}
-		</StoreContext.Consumer>
-	);
-};
 const mapStateToProps = (state) => {
 	return {
 		posts: state.profilePage.posts,
@@ -38,8 +12,12 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch) => {
 	return {
 		updateNewPostText: (text) => {
+			debugger;
 			let action = updateNewPostTextActionCreator(text);
 			dispatch(action);
+		},
+		addPost: () => {
+			dispatch(addPostActionCreatter());
 		},
 	};
 };
