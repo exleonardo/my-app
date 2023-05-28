c20bccd436a3791bc60bad41f333d56ff44bf7fd
Exleonardo <exleonardo@mail.ru>
2023-05-28 16:16:12 +0300

    add state to props

--- src/components/Profile/MyPosts/MyPostsContainer.jsx
+++ src/components/Profile/MyPosts/MyPostsContainer.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { addPostActionCreatter, updateNewPostTextActionCreator } from "../../../redux/profile-reducer";
 import MyPosts from "./MyPosts";
 import StoreContext from "../../../storeContext";
+import { connect } from "react-redux";
 
 const MyPostsContainer = (props) => {
 	return (
@@ -28,4 +29,20 @@ const MyPostsContainer = (props) => {
 		</StoreContext.Consumer>
 	);
 };
+const mapStateToProps = (state) => {
+	return {
+		posts: state.profilePage.posts,
+		newPostText: state.profilePage.newPostText,
+	};
+};
+const mapDispatchToProps = (dispatch) => {
+	return {
+		updateNewPostText: (text) => {
+			let action = updateNewPostTextActionCreator(text);
+			dispatch(action);
+		},
+	};
+};
+
+const MyPostsContainer = connect(mapStateToProps, mapDispatchToProps)(MyPosts);
 export default MyPostsContainer;
