d0b68a22c5a031c31d44ebc6d72713f1f29a1b47
Exleonardo <exleonardo@mail.ru>
2023-05-28 16:29:47 +0300

    del props

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import store from "./redux/store";
 import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-let rerenderEntireThree = (state) => {
+let rerenderEntireThree = () => {
 	root.render(
 		<React.StrictMode>
 			<Provider store={store}>
