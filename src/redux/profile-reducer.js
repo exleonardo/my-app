117d71f227152faf53a596b92f40537090658428
Exleonardo <exleonardo@mail.ru>
2023-05-29 00:01:04 +0300

    copy obj

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -12,8 +12,10 @@ let initialState = {
 const profileReducer = (state = initialState, action) => {
 	if (action.type === ADD_POST) {
 		let newPost = { id: 5, message: state.newPostText, likesCount: 0 };
-		state.posts.push(newPost);
-		state.newPostText = ``;
+		let stateCopy = { ...state };
+		stateCopy.posts = [...state.posts];
+		stateCopy.posts.push(newPost);
+		stateCopy.newPostText = ``;
 	} else if (action.type === UPDATE_NEW_POST_TEXT) {
 		state.newPostText = action.newText;
 	}
