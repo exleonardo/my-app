3c6a2d6b7240dffd42d189ecd6270fd31254009d
Exleonardo <exleonardo@mail.ru>
2023-05-28 11:27:12 +0300

    add Store context

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import store from "./redux/store";
+import StoreContext from "./storeContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 let rerenderEntireThree = (state) => {
 	root.render(
 		<React.StrictMode>
-			<App dispatch={store.dispatch.bind(store)} state={state} store={store} />
+			<StoreContext.Provider value={store}>
+				{/* dispatch={store.dispatch.bind(store)} state={state} store={store} */}
+				<App />
+			</StoreContext.Provider>
 		</React.StrictMode>
 	);
 };
