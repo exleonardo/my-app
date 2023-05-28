886b0ace24a0c7e099d1f44018c349d470f0d528
Exleonardo <exleonardo@mail.ru>
2023-05-29 00:11:06 +0300

    add state copy

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -11,14 +11,16 @@ let initialState = {
 
 const profileReducer = (state = initialState, action) => {
 	switch (action.type) {
-		case ADD_POST:
+		case ADD_POST: {
 			let newPost = { id: 5, message: state.newPostText, likesCount: 0 };
 			let stateCopy = { ...state };
 			stateCopy.posts = [...state.posts];
 			stateCopy.posts.push(newPost);
 			stateCopy.newPostText = ``;
 			return stateCopy;
+		}
 		case UPDATE_NEW_POST_TEXT:
+			let stateCopy = { ...state };
 			state.newPostText = action.newText;
 			return state;
 		default:
