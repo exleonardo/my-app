cd0086f066cdfe70f45f77e900e14cf77d7790f2
Exleonardo <exleonardo@mail.ru>
2023-05-29 00:28:22 +0300

    add case

--- src/redux/dialogs-reducer.js
+++ src/redux/dialogs-reducer.js
@@ -21,6 +21,7 @@ let initialState = {
 const dialogsReducer = (state = initialState, action) => {
 	switch (action.type) {
 		case UPDATE_NEW_MESSAGE_BODY:
+			state.newMessageBody = action.body;
 	}
 	if (action.type === UPDATE_NEW_MESSAGE_BODY) {
 		state.newMessageBody = action.body;
